e22448403bbf738526867538ebd7c4f4aeec2493
Oscar-Galindo <onmgalindo@gmail.com>
2025-07-03 13:33:25 -0400

    feat: add sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-
+import './categories.styles.scss';
 
 function App() {
+
+
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      description: "Category Description 1",
+    },
+    {
+      id: 2,
+      title: "Shirts",
+      description: "Category Description 1",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      description: "Category Description 1",
+    },
+    {
+      id: 4,
+      title: "Women",
+      description: "Category Description 1",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      description: "Category Description 1",
+    },
+  ];
   return (
-    <div>
-      <h1>Hello World</h1>
+    <>
+    <div className="categories-container">
+      {categories.map(({id, title, description}) => (
+        <div key={id} className="category-container">
+         <div className="background-image">
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        </div>
+      ))}
     </div>
+    </>
   );
 }
 
