e26e5fe79ad43d7c2d7eda00091bb12540e94db9
Oscar-Galindo <onmgalindo@gmail.com>
2025-07-03 22:57:09 -0400

    feat: added navigation to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-
+import Navigation from './components/routes/navigation/navigation.component';
 import Home from './components/routes/home/home.component';
+
+
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
